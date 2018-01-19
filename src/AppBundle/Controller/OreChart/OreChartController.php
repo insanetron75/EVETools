2cd7ec1157c2ac36dd2a85b4115319c993ccb986
insanetron75 <timmy_hasler@hotmail.com>
2018-01-19 02:42:13 +0000

    Basic Table Layout

--- src/AppBundle/Controller/OreChart/OreChartController.php
+++ src/AppBundle/Controller/OreChart/OreChartController.php
@@ -8,11 +8,17 @@
 
 namespace AppBundle\Controller\OreChart;
 
+use AppBundle\Entity\Invmarketgroups;
+use AppBundle\Entity\Invtypes;
+use Doctrine\Common\Persistence\ObjectManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class OreChartController extends Controller
 {
+    const ORE_PARENT_MARKET_GROUP = 54;
+    const MINERAL_GROUP_ID = 18;
+
     /**
      * @Route("/oreChart", name="ore_chart")
      *
@@ -20,8 +26,102 @@ class OreChartController extends Controller
      */
     public function showAction()
     {
+        $objectManager = $this->getDoctrine()->getManager();
+        $ores          = $this->getOres($objectManager);
+        $minerals      = $this->getMinerals($objectManager);
+        $ores          = $this->addMineralsToOre($ores, $minerals);
+        $ores          = $this->addRawIsk($ores);
+        $ores          = $this->addRefinedIsk($ores);
+
         return $this->render(
-            'oreChart/show.html.twig'
+            'oreChart/show.html.twig',
+            [
+                'ores'     => $ores,
+                'minerals' => $minerals
+            ]
         );
+    }
+
+    protected function getOres(ObjectManager $objectManager)
+    {
+        $oreMarketGroups = $objectManager->getRepository(Invmarketgroups::class)->findBy([
+            'parentgroupid' => self::ORE_PARENT_MARKET_GROUP
+        ]);
+        $ores            = $this->getOreNames($oreMarketGroups);
+
+        return $ores;
+    }
+
+    /**
+     * @param Invmarketgroups[] $oreMarketGroups
+     *
+     * @return array
+     */
+    protected function getOreNames(array $oreMarketGroups)
+    {
+        $ores = [];
+
+        foreach ($oreMarketGroups as $thisGroup) {
+            $ores[$thisGroup->getMarketgroupname()] = [
+                'name' => $thisGroup->getMarketgroupname()
+            ];
+        }
+
+        return $ores;
+    }
+
+    protected function getMinerals(ObjectManager $objectManager)
+    {
+        $mineralMarketGroups = $objectManager->getRepository(Invtypes::class)->findBy([
+            'groupid'   => self::MINERAL_GROUP_ID,
+            'published' => 1
+        ]);
+        $minerals            = $this->getMineralNames($mineralMarketGroups);
+
+        return $minerals;
+    }
+
+    /**
+     * @param Invtypes[] $mineralMarketGroups
+     *
+     * @return array
+     */
+    protected function getMineralNames(array $mineralMarketGroups)
+    {
+        $minerals = [];
+        foreach ($mineralMarketGroups as $thisType) {
+            $minerals[] = $thisType->getTypename();
+        }
+
+        return $minerals;
+    }
+
+    protected function addMineralsToOre($ores, $minerals)
+    {
+        foreach ($ores as $name => $ore) {
+            foreach ($minerals as $mineral) {
+                $ores[$name][$mineral] = 0;
+            }
+        }
+
+        return $ores;
+    }
+
+    public function addRawIsk($ores)
+    {
+        foreach ($ores as $name => $ore) {
+            $ores[$name]['raw'] = 0;
+        }
+
+        return $ores;
+    }
+
+    public function addRefinedIsk($ores)
+    {
+        foreach ($ores as $name => $ore) {
+            $ores[$name]['refined'] = 0;
+        }
+
+        return $ores;
     }
 }
