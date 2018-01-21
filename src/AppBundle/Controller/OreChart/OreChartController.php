d8f679e7ba4030d07d354798e0fe7718839d7154
insanetron75 <timmy_hasler@hotmail.com>
2018-01-21 02:52:41 +0000

    Added Raw and Refined isk  - currently defaults to the forge

--- src/AppBundle/Controller/OreChart/OreChartController.php
+++ src/AppBundle/Controller/OreChart/OreChartController.php
@@ -11,6 +11,7 @@ namespace AppBundle\Controller\OreChart;
 use AppBundle\Entity\Invmarketgroups;
 use AppBundle\Entity\Invtypes;
 use AppBundle\Entity\OreRefineAmounts;
+use AppBundle\Entity\Region;
 use Doctrine\Common\Persistence\ObjectManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -19,6 +20,9 @@ class OreChartController extends Controller
 {
     const ORE_PARENT_MARKET_GROUP = 54;
     const MINERAL_GROUP_ID = 18;
+
+    protected $itemPrices = [];
+    protected $selectedRegionId;
 
     /**
      * @Route("/oreChart", name="ore_chart")
@@ -31,8 +35,10 @@ class OreChartController extends Controller
         $ores          = $this->getOres($objectManager);
         $minerals      = $this->getMinerals($objectManager);
         $ores          = $this->addMineralsToOre($ores, $minerals);
-        $ores          = $this->addRawIsk($ores);
-        $ores          = $this->addRefinedIsk($ores);
+
+        $this->setSelectedRegion('The Forge');
+        $ores          = $this->addRawIsk($ores, $objectManager);
+        $ores          = $this->addRefinedIsk($ores, $objectManager);
 
         return $this->render(
             'oreChart/show.html.twig',
@@ -77,7 +83,8 @@ class OreChartController extends Controller
             'groupid'   => self::MINERAL_GROUP_ID,
             'published' => 1
         ]);
-        $minerals            = $this->getMineralNames($mineralMarketGroups);
+
+        $minerals = $this->populateMinerals($mineralMarketGroups);
 
         return $minerals;
     }
@@ -87,11 +94,13 @@ class OreChartController extends Controller
      *
      * @return array
      */
-    protected function getMineralNames(array $mineralMarketGroups)
+    protected function populateMinerals(array $mineralMarketGroups)
     {
         $minerals = [];
         foreach ($mineralMarketGroups as $thisType) {
-            $minerals[] = $thisType->getTypename();
+            $minerals[] = [
+                'name'  => $thisType->getTypename(),
+            ];
         }
 
         return $minerals;
@@ -101,7 +110,7 @@ class OreChartController extends Controller
     {
         foreach ($ores as $name => $ore) {
             foreach ($minerals as $mineral) {
-                $ores[$name][$mineral] = $this->getMineralAmountForOre($name, $mineral);
+                $ores[$name][$mineral['name']] = $this->getMineralAmountForOre($name, $mineral);
             }
         }
 
@@ -135,21 +144,73 @@ class OreChartController extends Controller
         }
     }
 
-    public function addRawIsk($ores)
+    public function addRawIsk($ores, ObjectManager $objectManager)
     {
         foreach ($ores as $name => $ore) {
-            $ores[$name]['raw'] = 0;
+            /** @var Invtypes $oreType */
+            $oreType            = $objectManager->getRepository(Invtypes::class)->findOneBy(
+                ['typename' => $name, 'published' => 1]
+            );
+            $rawIsk = ($this->getItemPrice($oreType) / $oreType->getVolume());
+            $ores[$name]['raw'] = number_format($rawIsk, 2);
         }
 
         return $ores;
     }
 
-    public function addRefinedIsk($ores)
+    public function addRefinedIsk($ores, ObjectManager $objectManager)
     {
-        foreach ($ores as $name => $ore) {
-            $ores[$name]['refined'] = 0;
+        foreach ($ores as $name => $minerals) {
+            /** @var Invtypes $oreType */
+            $oreType = $objectManager->getRepository(Invtypes::class)->findOneBy(
+                ['typename' => $name, 'published' => 1]
+            );
+            $refinedIsk = $this->calculateRefinedIsk($minerals, $oreType, $objectManager);
+            $ores[$name]['refined'] = number_format($refinedIsk, 2);
         }
 
         return $ores;
+    }
+
+    public function getItemPrice(Invtypes $item)
+    {
+        if (key_exists($item->getTypeid(), $this->itemPrices)) {
+            return $this->itemPrices[$item->getTypeid()];
+        }
+
+        $context    = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+        $url        = "https://api.eve-marketdata.com/api/item_prices2.xml?char_name=demo&type_ids={$item->getTypeid()}&region_ids={$this->selectedRegionId}&buysell=s";
+        $xml        = file_get_contents($url, false, $context);
+        $data       = simplexml_load_string($xml);
+        $priceArray = $data->xpath('/emd[@version="2"]/result/rowset[@name="item_prices"]/row[@buysell="s"]/@price');
+        $price      = json_decode($priceArray[0]);
+
+        return $this->itemPrices[$item->getTypeid()] = round($price, 2);
+    }
+
+    protected function calculateRefinedIsk(array $minerals, Invtypes $oreType, ObjectManager $objectManager)
+    {
+        $total = 0;
+        foreach ($minerals as $key => $quantity) {
+            if ($key === 'name' || $key === 'raw' || $quantity === '') {
+                continue;
+            }
+            /** @var Invtypes $mineralType */
+            $mineralType = $objectManager->getRepository(Invtypes::class)->findOneBy(
+                ['typename' => $key, 'published' => 1]
+            );
+            $amountPerM3 = $quantity / $oreType->getPortionsize() / $oreType->getVolume();
+            $total += ($this->getItemPrice($mineralType) * $amountPerM3);
+        }
+        return $total;
+    }
+
+    protected function setSelectedRegion($regionName = 'The Forge') {
+        /** @var Region $region */
+        $region = $this->getDoctrine()->getRepository(Region::class)->findOneBy([
+            'name' => $regionName
+        ]);
+
+        $this->selectedRegionId = $region->getId();
     }
 }
