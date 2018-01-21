af8b80e6e71a7003e50577d89262d17ebc62b664
insanetron75 <timmy_hasler@hotmail.com>
2018-01-21 03:26:56 +0000

    Quick Fix to order regions

--- src/AppBundle/Form/OreForm.php
+++ src/AppBundle/Form/OreForm.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Region;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,8 +17,13 @@ class OreForm extends AbstractType
             [
                 'class' => Region::class,
                 'label' => 'Choose Region',
-                'placeholder' => 'Select Region...'
+                'placeholder' => 'Select Region...',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('r')
+                              ->orderBy('r.name', 'ASC');
+                },
             ]
             );
     }
+
 }
